b3ed53751b7d322915a21dbdd11538883d995dc5
Aidil Kamal <aidilk.b20@mhs.unsyiah.ac.id>
2026-09-23 11:50:55 +0700

    add new arrow item: aoe ring and aoe curve line

--- src/data/arrowData.ts
+++ src/data/arrowData.ts
@@ -2,4 +2,6 @@ import type { DraggableItem } from "../types/planner";
 
 export const arrowList: DraggableItem[] = [
     { id: "arrow-Short", name: "Short Arrow", imagePath: "assets/Arrows/white-arrow.webp"},
+    { id: "arrow-Aoe-Ring", name: "AOE Ring", imagePath: "assets/Arrows/aoe-ring.png"},
+    { id: "arrow-Curve-Line", name: "Curve Line", imagePath: "assets/Arrows/curve-line.png"},
 ];
