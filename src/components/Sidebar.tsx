6536555124144c47f0ae75e236422b1647d94643
Aidil Kamal <aidilk.b20@mhs.unsyiah.ac.id>
2026-09-24 15:52:54 +0700

    change sidebar visuals to be collapsed on roles title first

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,25 +6,29 @@ import DraggableSurvivor from "./DraggableSurvivor"
 export default function Sidebar() {
     return (
         <div className="custom-scrollbar" style={{ height: "100vh", overflowY: "auto" }}>
-            <div className="sidebar-section">
-                <div className="sidebar-title">
-                    <p>HUNTER</p>
-                </div>
+            <details className="sidebar-section">
+                <summary>
+                    <div className="sidebar-title">
+                        <p>HUNTER</p>
+                    </div>
+                </summary>
                 <div id="sidebar-title-note">
                     <p>*click on the hunter card to reveal their abilities</p>
                 </div>
                 {hunterList.map((hunter) => (
                     <DraggableHunter key={hunter.id} hunter={hunter}/>
                 ))}
-            </div>
-            <div className="sidebar-section">
-                <div className="sidebar-title">
-                    <p>SURVIVOR</p>
-                </div>
+            </details>
+            <details className="sidebar-section">
+                <summary>
+                    <div className="sidebar-title">
+                        <p>SURVIVOR</p>
+                    </div>
+                </summary>
                 {survivorList.map((survivor) => (
                     <DraggableSurvivor key={survivor.id} survivor={survivor} />
                 ))}
-            </div>
+            </details>
         </div>
     )
 }
